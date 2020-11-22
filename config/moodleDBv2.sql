0e1cd417a1139c07f77e0c8f8061501b010eaf8e
NynaevaAlmera <45515196+NynaevaAlmera@users.noreply.github.com>
2020-11-22 02:38:55 +0100

    napravljen home.php

--- config/moodleDBv2.sql
+++ config/moodleDBv2.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 08, 2020 at 06:12 PM
--- Server version: 5.7.31
--- PHP Version: 7.3.21
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
