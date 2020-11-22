da76f77e18459373fa67a4fc72064fc9e4a3422e
mVujsic <mateja.vujsic@gmail.com>
2020-11-22 12:01:11 +0100

    login v0.4

--- config/moodleDBv2.sql
+++ config/moodleDBv2.sql
@@ -1,15 +1,4 @@
 -- phpMyAdmin SQL Dump
-<<<<<<< HEAD
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 08, 2020 at 06:12 PM
--- Server version: 5.7.31
--- PHP Version: 7.3.21
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-=======
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
@@ -20,7 +9,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
->>>>>>> master
 START TRANSACTION;
 SET time_zone = "+00:00";
 
